3fc797f29a4a0834222b3a96a18dff8a85cb9e68
Vidya Patil <vidyapatilr@gmail.com>
2026-09-06 11:40:26 -0400

    Disabled tracing since phoenix is running locally

--- tracing/setup.py
+++ tracing/setup.py
@@ -1,8 +1,11 @@
-from openinference.instrumentation.openai import OpenAIInstrumentor
+import os
 from phoenix.otel import register
+from openinference.instrumentation.openai import OpenAIInstrumentor
 
 
 def setup_tracing():
+    if os.getenv("DISABLE_TRACING"):
+        return
     register(
         project_name="agentaudit",
         endpoint="http://localhost:6006/v1/traces",
